6e02707cf90cb036e3923d91dbf3e33712b44a82
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-06-21 21:38:55 -0400

    Create UserSearch component with simply Graphql query for GitHub users

---
+++ components/UserSearch.tsx
@@ -0,0 +1,58 @@
+import { useQuery, gql } from '@apollo/client';
+
+const USER_SEARCH_QUERY = gql`
+	query UserSearch($username: String!) {
+		search(query: $username, type: USER, first: 10) {
+			userCount
+			edges {
+				cursor
+				node {
+					... on User {
+						id
+						login
+					}
+				}
+			}
+		}
+	}
+`;
+
+interface IUserSearchProps {
+	username: string;
+}
+
+const UserSearch = ({ username }: IUserSearchProps): JSX.Element => {
+	const { loading, error, data } = useQuery(USER_SEARCH_QUERY, {
+		variables: { username },
+	});
+
+	if (loading) {
+		return (
+			<div>
+				<p>Loading...</p>
+			</div>
+		);
+	}
+	if (error) {
+		return (
+			<div>
+				<p>Error!</p>
+				<p>{error}</p>
+			</div>
+		);
+	}
+
+	console.log(data);
+
+	return (
+		<div>
+			<ol>
+				{data.edges.map((node: any) => (
+					<li key={node.id}>{node.login}</li>
+				))}
+			</ol>
+		</div>
+	);
+};
+
+export default UserSearch;
