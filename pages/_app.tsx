b9744252776c1911e1c4319c6768339fab648fd0
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-06-25 10:00:04 -0400

    Add CSS reset and begin adding custom global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,12 @@ import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import client from '../apollo-client';
 
+import GlobalStyle from 'components/GlobalStyle';
+
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ApolloProvider client={client}>
+			<GlobalStyle />
 			<Component {...pageProps} />
 		</ApolloProvider>
 	);
