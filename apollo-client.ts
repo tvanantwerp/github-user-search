2769e548bcd3ef32e8cb5c29033f727b4eac592b
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-06-25 09:16:59 -0400

    Prepare to use styled-components

--- apollo-client.ts
+++ apollo-client.ts
@@ -7,7 +7,6 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
 	const token = process.env.NEXT_PUBLIC_GHPAT;
-	console.log(token);
 	return {
 		headers: {
 			...headers,
