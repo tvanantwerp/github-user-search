c602d554c47f1c60ab85985da7deb4e97026f17e
Tom VanAntwerp <tom@tomvanantwerp.com>
2021-06-27 12:11:23 -0400

    Move Head to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
 import client from '../apollo-client';
 
@@ -7,6 +8,20 @@ import GlobalStyle from 'components/GlobalStyle';
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ApolloProvider client={client}>
+			<Head>
+				<title>GitHub User Search</title>
+				<meta
+					name="description"
+					content="Search GitHub users by their username and explore data about them."
+				/>
+				<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="theme-color" content="#ffffff" />
+			</Head>
 			<GlobalStyle />
 			<Component {...pageProps} />
 		</ApolloProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import UserSearch from 'components/search/UserSearch';
 import { Context } from '@apollo/client';
 
@@ -8,26 +7,9 @@ import USER_SEARCH_QUERY from 'queries/UserSearchQuery';
 export default function Home({ data }: any) {
 	return (
 		<div>
-			<Head>
-				<title>GitHub User Search</title>
-				<meta
-					name="description"
-					content="Search GitHub users by their username and explore data about them."
-				/>
-				<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-				<link rel="manifest" href="/favicon/site.webmanifest" />
-				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-				<meta name="msapplication-TileColor" content="#da532c" />
-				<meta name="theme-color" content="#ffffff" />
-			</Head>
-
 			<main>
 				<UserSearch initialData={data} />
 			</main>
-
-			<footer></footer>
 		</div>
 	);
 }
